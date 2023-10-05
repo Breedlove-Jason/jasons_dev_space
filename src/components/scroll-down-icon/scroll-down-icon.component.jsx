3230baf6cd714dcd2dd9493a70b3aee94c6da9ff
j-breedlove <jbreedlove@jasonbreedlove.dev>
2023-10-05 15:48:05 -0700

    link scroll button to route

--- src/components/scroll-down-icon/scroll-down-icon.component.jsx
+++ src/components/scroll-down-icon/scroll-down-icon.component.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import {
   HomeScroll,
   HomeScrollButton,
@@ -8,9 +9,11 @@ import {
 function ScrollDownIcon() {
   return (
     <HomeScroll>
+        <Link to={"about"}>
       <HomeScrollButton href="#about">
         <StyledMouseIcon />
       </HomeScrollButton>
+        </Link>
     </HomeScroll>
   );
 }
