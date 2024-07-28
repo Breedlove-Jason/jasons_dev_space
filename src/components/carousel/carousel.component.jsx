2dd5879ddbe90d1678ddd23e392370fbacb4b5b3
Jason Breedlove <j@jasonbreedlove.com>
2024-07-28 00:04:26 -0700

    add calculator app

--- src/components/carousel/carousel.component.jsx
+++ src/components/carousel/carousel.component.jsx
@@ -5,44 +5,101 @@ import {
   MDBCarouselCaption,
 } from 'mdb-react-ui-kit';
 import './carousel.styles.css';
+import netflixPreview from '../../assets/images/netflix-preview.png';
+import crownClothingPreview from '../../assets/images/crown-clothing-preview.png';
+import calculatorPreview from '../../assets/images/calculator-preview.png';
 
 export default function Carousel() {
   return (
     <div className="carousel-container">
       <MDBCarousel showControls showIndicators>
         <MDBCarouselItem itemId={1}>
-          <img
-            src="https://mdbootstrap.com/img/new/slides/041.jpg"
-            className="d-block w-100"
-            alt="..."
-          />
+          <img src={netflixPreview} className="d-block w-100" alt="..." />
           <MDBCarouselCaption>
-            <h5>First slide label</h5>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            <h4>Netflix Clone</h4>
+            <p style={{ fontWeight: 'bold' }}>
+              The Netflix clone project is a full-featured streaming platform
+              that includes account management powered by Firebase, allowing
+              users to create, manage, and secure their profiles effortlessly.
+              The site integrates Stripe for seamless and secure payment
+              processing, enabling users to subscribe to different plans with
+              ease.
+            </p>
+            <a
+              href="https://netflix-clone-portfolio-52780fe750c8.herokuapp.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="preview-button"
+            >
+              Preview Here
+            </a>
           </MDBCarouselCaption>
         </MDBCarouselItem>
         <MDBCarouselItem itemId={2}>
-          <img
-            src="https://mdbootstrap.com/img/new/slides/042.jpg"
-            className="d-block w-100"
-            alt="..."
-          />
+          <img src={crownClothingPreview} className="d-block w-100" alt="..." />
           <MDBCarouselCaption>
-            <h5>Second slide label</h5>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <h4>Crown Clothing</h4>
+            <p style={{ fontWeight: 'bold' }}>
+              The Crown Clothing E-Shop is a modern e-commerce platform built
+              with React and Redux, featuring a seamless user sign-up and
+              sign-in process. It provides a smooth checkout experience,
+              allowing users to browse and purchase their favorite clothing
+              items effortlessly. This project demonstrates my expertise in
+              building dynamic and responsive web applications with robust state
+              management.
+            </p>
+            <a
+              href="https://jasonbreedlove.io"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="preview-button"
+            >
+              Preview Here
+            </a>
           </MDBCarouselCaption>
         </MDBCarouselItem>
         <MDBCarouselItem itemId={3}>
-          <img
-            src="https://mdbootstrap.com/img/new/slides/043.jpg"
-            className="d-block w-100"
-            alt="..."
-          />
+          <img src={calculatorPreview} className="d-block w-100" alt="..." />
           <MDBCarouselCaption>
-            <h5>Third slide label</h5>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            <h4>Calculator JS</h4>
+            <p style={{ fontWeight: 'bold' }}>
+              The JavaScript Calculator App is a sleek and responsive web
+              application that performs basic arithmetic operations with ease.
+              Built with vanilla JavaScript, HTML, and CSS, it features a
+              user-friendly interface and seamless functionality. This project
+              showcases my ability to create interactive and efficient web
+              applications.
             </p>
+            <a
+              href="https://calc-jscript.netlify.app/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="preview-button"
+            >
+              Preview Here
+            </a>
+          </MDBCarouselCaption>
+        </MDBCarouselItem>
+        <MDBCarouselItem itemId={3}>
+          <img src={calculatorPreview} className="d-block w-100" alt="..." />
+          <MDBCarouselCaption>
+            <h4>Pacman JS</h4>
+            <p style={{ fontWeight: 'bold' }}>
+              The JavaScript Pacman Game is an engaging and interactive web
+              application that replicates the classic arcade experience.
+              Developed with JavaScript, HTML, and CSS, it features smooth
+              animations, responsive controls, and authentic gameplay mechanics.
+              This project highlights my skills in game development and dynamic
+              web programming.
+            </p>
+            <a
+              href="https://calc-jscript.netlify.app/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="preview-button"
+            >
+              Preview Here
+            </a>
           </MDBCarouselCaption>
         </MDBCarouselItem>
       </MDBCarousel>
