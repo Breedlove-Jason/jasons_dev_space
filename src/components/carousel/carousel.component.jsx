61fc84baaa1c46081e542ee47082ca764aa8084c
Breedlove-Jason <j@jasonbreedlove.com>
2024-07-28 15:24:38 -0700

    add pacaman, and pig game

--- src/components/carousel/carousel.component.jsx
+++ src/components/carousel/carousel.component.jsx
@@ -1,13 +1,15 @@
-import React from 'react';
+import React from "react";
 import {
   MDBCarousel,
   MDBCarouselItem,
   MDBCarouselCaption,
-} from 'mdb-react-ui-kit';
-import './carousel.styles.css';
-import netflixPreview from '../../assets/images/netflix-preview.png';
-import crownClothingPreview from '../../assets/images/crown-clothing-preview.png';
-import calculatorPreview from '../../assets/images/calculator-preview.png';
+} from "mdb-react-ui-kit";
+import "./carousel.styles.css";
+import netflixPreview from "../../assets/images/netflix-preview.png";
+import crownClothingPreview from "../../assets/images/crown-clothing-preview.png";
+import calculatorPreview from "../../assets/images/calculator-preview.png";
+import pacmanPreview from "../../assets/images/pacman-preview.png";
+import pigGamePreview from "../../assets/images/pig-game-preview.png";
 
 export default function Carousel() {
   return (
@@ -17,7 +19,7 @@ export default function Carousel() {
           <img src={netflixPreview} className="d-block w-100" alt="..." />
           <MDBCarouselCaption>
             <h4>Netflix Clone</h4>
-            <p style={{ fontWeight: 'bold' }}>
+            <p style={{ fontWeight: "bold" }}>
               The Netflix clone project is a full-featured streaming platform
               that includes account management powered by Firebase, allowing
               users to create, manage, and secure their profiles effortlessly.
@@ -39,7 +41,7 @@ export default function Carousel() {
           <img src={crownClothingPreview} className="d-block w-100" alt="..." />
           <MDBCarouselCaption>
             <h4>Crown Clothing</h4>
-            <p style={{ fontWeight: 'bold' }}>
+            <p style={{ fontWeight: "bold" }}>
               The Crown Clothing E-Shop is a modern e-commerce platform built
               with React and Redux, featuring a seamless user sign-up and
               sign-in process. It provides a smooth checkout experience,
@@ -59,10 +61,32 @@ export default function Carousel() {
           </MDBCarouselCaption>
         </MDBCarouselItem>
         <MDBCarouselItem itemId={3}>
+          <img src={pacmanPreview} className="d-block w-100" alt="..." />
+          <MDBCarouselCaption>
+            <h4>Pacman JS</h4>
+            <p style={{ fontWeight: "bold" }}>
+              The JavaScript Pacman Game is an engaging and interactive web
+              application that replicates the classic arcade experience.
+              Developed with JavaScript, HTML, and CSS, it features smooth
+              animations, responsive controls, and authentic gameplay mechanics.
+              This project highlights my skills in game development and dynamic
+              web programming.
+            </p>
+            <a
+              href="https://pacman-js-build.netlify.app"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="preview-button"
+            >
+              Preview Here
+            </a>
+          </MDBCarouselCaption>
+        </MDBCarouselItem>
+        <MDBCarouselItem itemId={4}>
           <img src={calculatorPreview} className="d-block w-100" alt="..." />
           <MDBCarouselCaption>
             <h4>Calculator JS</h4>
-            <p style={{ fontWeight: 'bold' }}>
+            <p style={{ fontWeight: "bold" }}>
               The JavaScript Calculator App is a sleek and responsive web
               application that performs basic arithmetic operations with ease.
               Built with vanilla JavaScript, HTML, and CSS, it features a
@@ -81,19 +105,20 @@ export default function Carousel() {
           </MDBCarouselCaption>
         </MDBCarouselItem>
         <MDBCarouselItem itemId={3}>
-          <img src={calculatorPreview} className="d-block w-100" alt="..." />
+          <img src={pigGamePreview} className="d-block w-100" alt="..." />
           <MDBCarouselCaption>
-            <h4>Pacman JS</h4>
-            <p style={{ fontWeight: 'bold' }}>
-              The JavaScript Pacman Game is an engaging and interactive web
-              application that replicates the classic arcade experience.
-              Developed with JavaScript, HTML, and CSS, it features smooth
-              animations, responsive controls, and authentic gameplay mechanics.
-              This project highlights my skills in game development and dynamic
-              web programming.
+            <h4>Pig Game JS</h4>
+            <p style={{ fontSize: "15px", fontWeight: "bold" }}>
+              A fun and interactive implementation of the classic Pig Dice Game,
+              where two players compete to reach 100 points first. Players take
+              turns rolling a die and accumulating points, but must decide
+              whether to risk rolling again or bank their points before rolling
+              a 1, which would forfeit their turn's points. Built with HTML,
+              CSS, and JavaScript, this project demonstrates my ability to
+              create engaging and dynamic web applications.
             </p>
             <a
-              href="https://calc-jscript.netlify.app/"
+              href="https://pig-game-jscript.netlify.app"
               target="_blank"
               rel="noopener noreferrer"
               className="preview-button"
